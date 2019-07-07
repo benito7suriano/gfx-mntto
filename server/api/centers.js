fe1d95d23685083e6c881b4d4da368c925c80768
Beno Suriano <bjsuriano@gmail.com>
2019-07-06 23:36:42 -0600

    GET routes for centers wired up.

---
+++ server/api/centers.js
@@ -0,0 +1,78 @@
+const router = require('express').Router()
+const { Center, RollformerRoof, RollformerGHT, Forklift } = require('../db')
+module.exports = router
+
+// GET /api/centros
+router.get('/', async (req,res,next) => {
+  try {
+    const centers = await Center.findAll()
+    res.json(centers)
+  } catch (error) { next(err) }
+})
+
+// GET /api/centros/:centroId
+router.get(`/:centroId`, async (req, res, next) => {
+  const { centroId } = req.params
+
+  try {
+    const center = await Center.findByPk(centroId)
+
+    if(!center) {
+      res.sendStatus(404)
+    } else {
+      res.json(center)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET /api/centers/:centerId/roof-rollformer
+router.get(`/:centerId/roof-rollformer`, async (req, res, next) => {
+  const { centerId } = req.params
+
+  try {
+    const rfm = await RollformerRoof.findOne({
+      where: {
+        centerId: centerId
+      },
+      include: [{
+        model: Center
+      }]
+    })
+
+    if(!rfm) {
+      res.sendStatus(404)
+    } else {
+      res.json(rfm)
+    }
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET /api/centers/:centerId/roof-rollformer
+router.get(`/:centerId/ght-rollformer`, async (req, res, next) => {
+  const { centerId } = req.params
+
+  try {
+    const rfm = await RollformerGHT.findOne({
+      where: {
+        centerId: centerId
+      },
+      include: [{
+        model: Center
+      }]
+    })
+
+    if(!rfm) {
+      res.sendStatus(404)
+    } else {
+      res.json(rfm)
+    }
+
+  } catch (error) {
+    next(error)
+  }
+})
