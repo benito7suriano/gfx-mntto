29b4518eec485bea4f7e52ef189a4c2843db26f0
Beno Suriano <bjsuriano@gmail.com>
2019-07-06 20:58:54 -0600

    Fixed PATH bug.

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,14 @@
 const router = require('express').Router()
+module.exports = router
 
-router.get('/')
+router.use('/paises', require('./countries')) // countries API
+// router.use('/centros') // centers API
+// router.use('/techos-maquinas') // roof-rollformers API
+// router.use('/polin-maquinas') // ght-rollformers API
+// router.use('/montacargas') // forklifts API
+
+router.use((req, res, next) => {
+  const err = new Error('API route not found!')
+  err.status = 404
+  next(err)
+})
