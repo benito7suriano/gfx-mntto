ab3d48e15f81e61f7583398dc67cd6aa65db4dcc
Beno Suriano <bjsuriano@gmail.com>
2019-07-06 20:55:11 -0600

    GET routes for countries wired up.

---
+++ server/api/countries.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+const { Country } = require('../db')
+module.exports = router
+
+// GET /api/paises
+router.get('/', async (req,res,next) => {
+  try {
+    const paises = await Country.findAll()
+    res.json(paises)
+  } catch (error) { next(err) }
+})
+
+// GET /api/paises/:paisId
+router.get(`/:paisId`, async (req, res, next) => {
+  const { paisId } = req.params
+
+  console.log('paisId:', paisId)
+
+  try {
+    const pais = await Country.findByPk(paisId)
+
+    if(!pais) {
+      res.sendStatus(404)
+    } else {
+      res.json(pais)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
